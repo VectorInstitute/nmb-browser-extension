6adc4752aa025bff47080c29e90af5dbb9d533cf
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2024-10-07 17:20:06 -0400

    Calculating rile index properly

--- content-script/App.tsx
+++ content-script/App.tsx
@@ -9,7 +9,8 @@ import winkNLP from "wink-nlp";
 import winkModel from "wink-eng-lite-web-model";
 
 const App = () => {
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState(null);
+    const [rileIndex, setRileIndex] = useState(null);
     const [loading, setLoading] = useState(false);
 
     // Differences in Manifest V2 vs V3:
@@ -25,11 +26,14 @@ const App = () => {
 
     async function handleOnClick() {
         setLoading(true);
-        setResult("");
+        setResult(null);
+        setRileIndex(null);
         try {
             const reader = new Readability(document.cloneNode(true));
             const articleContent = reader.parse();
             const textContent = articleContent.textContent;
+
+            console.log(textContent);
 
             let winkWorker = winkNLP(winkModel);
             const sentences = getSentences(textContent, winkWorker);
@@ -37,17 +41,22 @@ const App = () => {
             let { session, tokenizer } = await getSessionAndTokenizer(modelPath, tokenizerPath, tokenizerConfigPath);
             const modelInputs = await getInputs(sentences, tokenizer, session.inputNames);
 
+            const results = []
             for (let modelInput of modelInputs) {
-                const result = await session.run(modelInput);
-                console.log(result);
+                results.push(await session.run(modelInput));
+                console.log(`${results.length}/${modelInputs.length}`);
             }
+
+            const rile = calculateRILEIndex(results);
+            const label = LABEL_FOR_INDEX.reduce((l, c) => rile <= c.upperBound && rile > c.lowerBound ? c : l);
+
+            setResult(`Bias: ${label.label}.`);
+            setRileIndex(`RILE index: ${(rile).toLocaleString("en-US", { minimumFractionDigits: 2 })}`);
 
             // Ensure heavy objects are deallocated
             winkWorker = null;
             session = null;
             tokenizer = null;
-
-            setResult(`Page text content has ${textContent.length} characters. ONNX session: ${session}`);
         } catch (error) {
             console.error(error);
             setResult(error.toString());
@@ -63,7 +72,11 @@ const App = () => {
               className='px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm disabled:opacity-75 w-48'
               disabled={loading} onClick={handleOnClick}>Analyze
             </button>
-            <p>{result}</p>
+            <p>
+                {result}
+                <br/>
+                {rileIndex}
+            </p>
           </div>
         </div>
     );
@@ -123,3 +136,35 @@ function buildSentenceContext(sentences: Array<string>, index: number): string {
     }
     return sentences[index - 1] + " " + sentences[index] + " " + sentences[index + 1];
 }
+
+function calculateRILEIndex(results: Array<Object>): number {
+    const countLeft = new Array(results.length).fill(0);
+    const countRight = new Array(results.length).fill(0);
+    for (let result of results) {
+        const logits = result.logits.cpuData;
+        const argmax = logits.reduce((maxIndex, current, i, logits) => current > logits[maxIndex] ? i : maxIndex, 0);
+
+        const leftIndex = LEFT_LEANING_CATEGORIES.indexOf(argmax);
+        const rightIndex = RIGHT_LEANING_CATEGORIES.indexOf(argmax);
+        if (leftIndex >= 0) {
+            countLeft[leftIndex] += 1;
+        }
+        if (rightIndex >= 0) {
+            countRight[rightIndex] += 1;
+        }
+    }
+
+    const leftCumulativeProb = countLeft.reduce((sum, current) => sum + ((current/results.length) * 100), 0);
+    const rightCumulativeProb = countRight.reduce((sum, current) => sum + ((current/results.length) * 100), 0);
+    return rightCumulativeProb - leftCumulativeProb;
+}
+
+const LEFT_LEANING_CATEGORIES = [3, 5, 6, 7, 12, 22, 23, 25, 31, 32, 39, 41, 51];
+const RIGHT_LEANING_CATEGORIES = [4, 11, 13, 19, 20, 21, 26, 33, 40, 43, 45, 47, 48];
+const LABEL_FOR_INDEX = [
+    {  lowerBound: -30, upperBound: -100, label: "Left" },
+    {  lowerBound: -29, upperBound: 10, label: "Center-Left" },
+    {  lowerBound: 20, upperBound: 11, label: "Center" },
+    {  lowerBound: 39, upperBound: 21, label: "Center-Right" },
+    {  lowerBound: 40, upperBound: 100, label: "Right" },
+];
